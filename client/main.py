4c2b69d5d44b384a433726cf6fab7d9a6d398ce2
Nguyen Thanh Loc <loc08122004@gmail.com>
2026-01-15 15:36:43 +0700

    set database by mysql

--- client/main.py
+++
@@ -1,5 +0,0 @@
-from client import RockPaperScissorsClient
-
-if __name__ == "__main__":
-    app = RockPaperScissorsClient()
-    app.run()
--- server/main.py
+++
@@ -1,4 +0,0 @@
-from server import GameServer
-
-if __name__ == "__main__":
-    GameServer().start()
